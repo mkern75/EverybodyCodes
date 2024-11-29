a8580b41f0fa937e5554b3c0002cdefe6dfcac1f
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2024-11-29 15:41:27 +0000

    day 19

--- TheKingdomOfAlgorithmia2024/q19.py
+++ TheKingdomOfAlgorithmia2024/q19.py
@@ -1,69 +1,102 @@
 from time import time
 
 N8 = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
+SHOW_GRID = True
 
 
-def rotate(dir, grid, r, c):
+def ff(r, c, m):
+    return r * m + c
+
+
+def fr(x, m):
+    return divmod(x, m)
+
+
+def rotate(dir, grid, r_centre, c_centre):
+    assert dir in "LR"
     offset = 1 if dir == "L" else -1
     mem = []
     for i in range(8):
-        mem += [grid[r + N8[i][0]][c + N8[i][1]]]
+        mem += [grid[r_centre + N8[i][0]][c_centre + N8[i][1]]]
     for i in range(8):
-        grid[r + N8[i][0]][c + N8[i][1]] = mem[(i + offset) % 8]
+        grid[r_centre + N8[i][0]][c_centre + N8[i][1]] = mem[(i + offset) % 8]
 
 
 def calc_mapping(grid, key):
     n, m = len(grid), len(grid[0])
-    grid_other = [[] for _ in range(n)]
+
+    gg = [[0] * m for _ in range(n)]
     for r in range(n):
         for c in range(m):
-            grid_other[r] += [(r, c)]
+            gg[r][c] = ff(r, c, m)
+
     i = 0
     for r in range(1, n - 1):
         for c in range(1, m - 1):
-            rotate(key[i], grid_other, r, c)
+            rotate(key[i], gg, r, c)
             i = (i + 1) % len(key)
-    mapping = {}
+
+    mapping = [[0] * m for _ in range(n)]
     for r in range(n):
         for c in range(m):
-            rr, cc = grid_other[r][c]
-            mapping[rr, cc] = r, c
+            rr, cc = fr(gg[r][c], m)
+            mapping[rr][cc] = ff(r, c, m)
     return mapping
 
 
-def find_cycle(mapping, rs, cs):
-    cycle = [(rs, cs)]
+def find_cycle(rs, cs, mapping):
+    m = len(mapping[0])
+    cycle = [ff(rs, cs, m)]
     r, c = rs, cs
     while True:
-        r, c = mapping[r, c]
+        r, c = fr(mapping[r][c], m)
         if r == rs and c == cs:
             break
-        cycle += [(r, c)]
+        cycle += [ff(r, c, m)]
     return cycle
 
 
 def calc_cycles(grid, key):
     n, m = len(grid), len(grid[0])
-    cycles = [[[] for _ in range(m)] for _ in range(n)]
     mapping = calc_mapping(grid, key)
+    cycles = []
+    cycle_id = [[-1] * m for _ in range(n)]
+    cycle_pos = [[-1] * m for _ in range(n)]
     for r in range(n):
         for c in range(m):
-            cycles[r][c].extend(find_cycle(mapping, r, c))
-    return cycles
+            if cycle_id[r][c] == -1:
+                cycle = find_cycle(r, c, mapping)
+                id = len(cycles)
+                cycles += [cycle]
+                for pos, x in enumerate(cycle):
+                    rr, cc = fr(x, m)
+                    cycle_id[rr][cc] = id
+                    cycle_pos[rr][cc] = pos
+    return cycles, cycle_id, cycle_pos
 
 
 def simulate(grid, key, cnt):
     n, m = len(grid), len(grid[0])
+
     res = [["."] * m for _ in range(n)]
-    cycles = calc_cycles(grid, key)
+    cycles, cycle_id, cycle_pos = calc_cycles(grid, key)
     for r in range(n):
         for c in range(m):
-            rr, cc = cycles[r][c][cnt % len(cycles[r][c])]
+            id = cycle_id[r][c]
+            pos = cycle_pos[r][c]
+            rr, cc = fr(cycles[id][(pos + cnt) % len(cycles[id])], m)
             res[rr][cc] = grid[r][c]
+
+    if SHOW_GRID:
+        print()
+        for row in res:
+            print("".join(row))
+
     for row in res:
         if ">" in row and "<" in row:
             s = "".join(row)
             return s[s.index(">") + 1: s.index("<")]
+
     assert False
 
 
