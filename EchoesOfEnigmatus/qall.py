bdfb4ad7f40214a808e487c71adfb97c43084114
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2025-06-07 10:06:38 +0100

    rename day to quest

--- EchoesOfEnigmatus/qall.py
+++ EchoesOfEnigmatus/qall.py
@@ -2,9 +2,9 @@ from time import time
 
 overall_start = time()
 
-for day in range(1, 4):
-    print(f"Day {day}")
-    exec(open(f"./EchoesOfEnigmatus/q{day:02d}.py").read())
+for quest in range(1, 4):
+    print(f"Quest {quest}")
+    exec(open(f"./EchoesOfEnigmatus/q{quest:02d}.py").read())
     print(f"running time so far: {time() - overall_start:.3f}s")
     print()
 
