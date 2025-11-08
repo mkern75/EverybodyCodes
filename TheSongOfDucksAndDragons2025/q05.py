b221b0bfa3b401e4c22d6229c955856994f283ef
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2025-11-08 01:51:19 +0000

    day 5 restructured

--- TheSongOfDucksAndDragons2025/q05.py
+++ TheSongOfDucksAndDragons2025/q05.py
@@ -1,6 +1,9 @@
 from time import time
 from collections import namedtuple
 
+# ********************************* part 1
+time_start = time()
+
 Sword = namedtuple("sword", "id fishbone")
 
 
@@ -25,6 +28,25 @@ def calc_quality(sword: Sword) -> int:
     return int("".join(map(str, [seg[1] for seg in sword.fishbone])))
 
 
+INPUT_FILE = "./data/q05_p1.txt"
+data = [line.rstrip('\n') for line in open(INPUT_FILE, "r")]
+sword = load_sword(data[0])
+ans1 = calc_quality(sword)
+print(f"part 1: {ans1}  ({time() - time_start:.3f}s)")
+
+# ********************************* part 2
+time_start = time()
+
+INPUT_FILE = "./data/q05_p2.txt"
+data = [line.rstrip('\n') for line in open(INPUT_FILE, "r")]
+quality = [calc_quality(load_sword(line)) for line in data]
+ans2 = max(quality) - min(quality)
+print(f"part 2: {ans2}  ({time() - time_start:.3f}s)")
+
+# ********************************* part 3
+time_start = time()
+
+
 def evaluate(sword: Sword) -> tuple:
     res = [calc_quality(sword)]
     res += [int("".join(map(str, [x for x in seg if x is not None]))) for seg in sword.fishbone]
@@ -32,29 +54,8 @@ def evaluate(sword: Sword) -> tuple:
     return tuple(res)
 
 
-# ********************************* part 1
-time_start = time()
-INPUT_FILE = "./data/q05_p1.txt"
-data = [line.rstrip('\n') for line in open(INPUT_FILE, "r")]
-
-sword = load_sword(data[0])
-ans1 = calc_quality(sword)
-print(f"part 1: {ans1}  ({time() - time_start:.3f}s)")
-
-# ********************************* part 2
-time_start = time()
-INPUT_FILE = "./data/q05_p2.txt"
-data = [line.rstrip('\n') for line in open(INPUT_FILE, "r")]
-
-quality = [calc_quality(load_sword(line)) for line in data]
-ans2 = max(quality) - min(quality)
-print(f"part 2: {ans2}  ({time() - time_start:.3f}s)")
-
-# ********************************* part 3
-time_start = time()
 INPUT_FILE = "./data/q05_p3.txt"
 data = [line.rstrip('\n') for line in open(INPUT_FILE, "r")]
-
 evals = sorted((evaluate(load_sword(line)) for line in data), reverse=True)
 ans3 = sum(ev[-1] * i for i, ev in enumerate(evals, start=1))
 print(f"part 3: {ans3}  ({time() - time_start:.3f}s)")
