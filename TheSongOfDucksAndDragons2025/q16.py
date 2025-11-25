5c8b682fd655b491b86ca5df5209883a178de7c4
Mathias Kern <82466426+mkern75@users.noreply.github.com>
2025-11-25 03:11:10 +0000

    day 16 streamlining

--- TheSongOfDucksAndDragons2025/q16.py
+++ TheSongOfDucksAndDragons2025/q16.py
@@ -17,16 +17,14 @@ time_start = time()
 INPUT_FILE = "./data/q16_p2.txt"
 data = [line.rstrip('\n') for line in open(INPUT_FILE, "r")]
 
-nums = list(map(int, data[0].split(",")))
-n_cols = len(nums)
+cols = list(map(int, data[0].split(",")))
+n_cols = len(cols)
 spell = []
-cnt = [0] * n_cols
-for i, x in enumerate(nums):
-    if x > cnt[i]:
+for i in range(n_cols):
+    if cols[i] > 0:
         spell += [i + 1]
-        for j in range(n_cols):
-            if (j + 1) % (i + 1) == 0:
-                cnt[j] += 1
+        for j in range(i, n_cols, i + 1):
+            cols[j] -= 1
 ans2 = prod(spell)
 
 print(f"part 2: {ans2}  ({time() - time_start:.3f}s)")
@@ -36,19 +34,17 @@ time_start = time()
 INPUT_FILE = "./data/q16_p3.txt"
 data = [line.rstrip('\n') for line in open(INPUT_FILE, "r")]
 
-nums = list(map(int, data[0].split(",")))
-n_cols = len(nums)
+cols = list(map(int, data[0].split(",")))
+n_cols = len(cols)
 spell = []
-cnt = [0] * n_cols
-for i, x in enumerate(nums):
-    if x > cnt[i]:
+for i in range(n_cols):
+    if cols[i] > 0:
         spell += [i + 1]
-        for j in range(n_cols):
-            if (j + 1) % (i + 1) == 0:
-                cnt[j] += 1
+        for j in range(i, n_cols, i + 1):
+            cols[j] -= 1
 
 total = 202520252025000
-lo, hi = 0, 1 << 127
+lo, hi = 0, 1 << 63
 while lo + 1 < hi:
     mid = (lo + hi) // 2
     if sum(mid // x for x in spell) <= total:
